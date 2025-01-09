033f18d2c92899a31f06711eaa8e99daee6b3996
zethboy <yoginugraha842@gmail.com>
2025-01-09 23:46:27 +0700

    add nav li

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,9 @@
 // hamburgerr
 const hamburger = document.querySelector('#hamburger')
+const navmenu = document.querySelector('#nav-menu')
 hamburger.addEventListener('click', function( ){
   hamburger.classList.toggle('hamburger-active');
+  navmenu.classList.toggle('hidden');
 })
 
 // navbar fixed
