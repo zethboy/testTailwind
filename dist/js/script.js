7b89589bb5a0ae5cb9026dda7e0f905832cc40cb
zethboy <yoginugraha842@gmail.com>
2025-01-09 23:29:40 +0700

    add backdropfilter in navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,3 +3,15 @@ const hamburger = document.querySelector('#hamburger')
 hamburger.addEventListener('click', function( ){
   hamburger.classList.toggle('hamburger-active');
 })
+
+// navbar fixed
+window.onscroll = function ( ){
+  const header = document.querySelector('header')
+  const fixednav = header.offsetTop;
+
+  if(window.pageYOffset > fixednav){
+    header.classList.add('navbar-fixed')
+  }else {
+    header.classList.remove(navbar-fixed)
+  }
+}
